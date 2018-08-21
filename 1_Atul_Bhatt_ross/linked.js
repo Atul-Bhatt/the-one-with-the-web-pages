81d4d023f31d07c53eb83d8d617c2d2799151283
atul-bhatt <atulbhatt61@gmail.com>
2018-08-21 21:58:25 +0530

    first html document

---
+++ 1_Atul_Bhatt_ross/linked.js
@@ -0,0 +1,9 @@
+function loading(){
+	document.getElementById('body').style.backgroundColor = "teal";
+}
+function rossHappy(){
+	document.getElementById('rossnormal').src="ross.jpg";
+}
+function rossAngry() {
+	document.getElementById('rossnormal').src="rossangry.jpg";
+}
